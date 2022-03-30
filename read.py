3b7b36e37e485d75e02a65bf2b2a556ed22fd6e4
Tanya <thetwtanya@gmail.com>
2022-03-30 12:24:00 +0800

    list comprehension

--- read.py
+++ read.py
@@ -10,23 +10,34 @@ print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 sum_len = 0
 for d in data:
-	sum_len += len(d)
+    sum_len += len(d)
 
 print('留言的平均長度為', sum_len/len(data))
 
 
 new = []
 for d in data:
-	if len(d) < 100:
-		new.append(d)
+    if len(d) < 100:
+        new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
 
-good = []
+# good = []
+# for d in data:
+#   if 'good' in d:
+#       good.append(d)
+# print('一共有', len(good), '筆留言提到good')
+
+good = [1 for d in data if 'good' in d]
+print(good)
+
+
+bad = ['bad' in d for d in data]
+print(bad)
+
+bad = []
 for d in data:
-    if 'good' in d:
-        good.append(d)
-print('一共有', len(good), '筆留言提到good')
-print(good[0])
+    bad.append('bad' in d)
+
